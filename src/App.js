95f4d3ff099f3a28465757318ce7b8f205b3e9a4
Rameez khan <sardarrameez5555@gmail.com>
2020-06-13 21:06:19 +0500

    Add new component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>Rameez
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(prop) {
+return <div>Hello World from {prop.name} and age is {prop.age}
+<br/>
+<Hello firstname="Sardar Rameez"></Hello>
+</div>
 }
+
 
 export default App;
---
+++ src/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+function Hello({firstname}) {
+return <p>This is para in hello.js from {firstname}</p>;
+}
+
+export default Hello;
